426ed27e77756626d3ba7f850526ee7e6053eea9
andres-suarez-itmo <48585577+andres-suarez-itmo@users.noreply.github.com>
2019-06-06 16:58:32 +0300

    Add files via upload

--- scrapper/pnp_3.py
+++ scrapper/pnp_3.py
@@ -1,4 +1,5 @@
 import scrapy
+from urlparse import urlparse
 import json
 decoder = json.JSONDecoder()
 
@@ -26,11 +27,19 @@ class QuotesSpider(scrapy.Spider):
 	start_urls = list_urls
 
 	def parse(self, response):
+		url_parsed = urlparse(response.xpath('//meta[@property=$val]/@content', val='og:url').get())
+		path =  url_parsed.path
+		int_a = path.find('/')
+		int_a = int_a + 1
+		int_b = path.find('/',int_a)
+		print path[int_a:int_b]
 		yield {
 			'title': response.xpath('//meta[@property=$val]/@content', val='og:title').get(),
 			'href': response.xpath('//meta[@property=$val]/@content', val='og:url').get(),
 			'image': response.xpath('//meta[@property=$val]/@content', val='og:image').get(),
 			'description': response.xpath('//meta[@property=$val]/@content', val='og:description').get(),
-			'text': response.css('div.js-mediator-article').get()
+			'text': response.css('div.js-mediator-article').get(),
+			'section': path[int_a:int_b]
 		}
 
+#response.xpath('//meta[@property=$val]/@content', val='og:title').get()
